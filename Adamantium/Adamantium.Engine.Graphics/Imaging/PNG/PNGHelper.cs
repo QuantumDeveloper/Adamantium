1a2262f55a54ba76edf72e9d7f6aa8f00bd5a624
Denis Zaporozhets <mderkins@gmail.com>
2019-05-24 00:44:39 +0300

    Start working on PNG implemenetation

---
+++ Adamantium/Adamantium.Engine.Graphics/Imaging/PNG/PNGHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Adamantium.Engine.Graphics.Imaging.PNG
+{
+    public static class PNGHelper
+    {
+        internal static byte[] PngHeader = { 137, 80, 78, 71, 13, 10, 26, 10 };
+
+        public static unsafe Image LoadFromPNGMemory(IntPtr pSource, int size, bool makeACopy, GCHandle? handle)
+        {
+            var stream = new UnmanagedMemoryStream((byte*)pSource, size);
+            PNGDecoder decoder = new PNGDecoder(stream);
+            decoder.Decode();
+            return null;
+        }
+
+        public static void SavePngToMemory(PixelBuffer[] pixelBuffers, int count, ImageDescription description, Stream imageStream)
+        {
+
+        }
+    }
+}
