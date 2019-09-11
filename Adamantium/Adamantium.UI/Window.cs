7283df26297bdfbde94d137ab62cc2f172f2240c
Denis Zaporozhets <mderkins@gmail.com>
2019-09-11 17:35:19 +0300

    Fixed compilation errors

--- Adamantium/Adamantium.UI/Window.cs
+++ Adamantium/Adamantium.UI/Window.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Runtime.InteropServices;
+using Adamantium.Mathematics;
 using Adamantium.UI.Controls;
 using Adamantium.UI.Windows;
 
@@ -16,6 +18,21 @@ namespace Adamantium.UI
             {
                 return new OSXWindow();
             }
+            
+            throw new NotSupportedException("This ");
         }
+
+        public abstract Point PointToClient(Point point);
+        public abstract Point PointToScreen(Point point);
+        public abstract void Show();
+        public abstract void Close();
+        public abstract void Hide();
+        public abstract IntPtr Handle { get; internal set; }
+        public abstract bool IsClosed { get; protected set; }
+        public abstract int ClientWidth { get; set; }
+        public abstract int ClientHeight { get; set; }
+        public abstract event EventHandler<SizeChangedEventArgs> ClientSizeChanged;
+        public abstract event EventHandler<WindowClosingEventArgs> Closing;
+        public abstract event EventHandler<EventArgs> Closed;
     }
 }
