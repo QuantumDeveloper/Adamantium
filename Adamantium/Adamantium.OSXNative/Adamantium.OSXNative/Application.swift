e50e6b8022ccfdf2e6e49272365d09c3649aba2b
Denis Zaporozhets <mderkins@gmail.com>
2019-11-05 16:02:46 +0200

    Added new macOS app interop project on SWIFT

---
+++ Adamantium/Adamantium.OSXNative/Adamantium.OSXNative/Application.swift
@@ -0,0 +1,13 @@
+//
+//  Application.swift
+//  Adamantium.OSXNative
+//
+//  Created by Denys Zaporozhets on 02.11.2019.
+//  Copyright © 2019 Denys Zaporozhets. All rights reserved.
+//
+
+import Foundation
+import AppKit
+import QuartzCore
+import Metal
+import "Application.h"
