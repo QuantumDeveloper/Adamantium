0d7dfa10af2cad0251cd57f4bf86bc64868a897b
black_joker <mderkins@gmail.com>
2022-07-08 15:22:58 +0300

    Add missing files

---
+++ Adamantium/Adamantium.UI/EventTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Adamantium.UI.Input;
+using Adamantium.UI.RoutedEvents;
+
+namespace Adamantium.UI;
+
+public class EventTrigger : ITrigger
+{
+    private IInputComponent component;
+    private Dictionary<AdamantiumProperty, object> values;
+    private bool applied;
+   
+    public RoutedEvent Event { get; set; }
+    
+    public SetterCollection Setters { get; set; }
+    public void Apply(IInputComponent control)
+    {
+        component = control;
+        values = new Dictionary<AdamantiumProperty, object>();
+        applied = false;
+        component.AddHandler(Event, new RoutedEventHandler(EventHandler));
+    }
+
+    private void EventHandler(object sender, RoutedEventArgs e)
+    {
+        
+    }
+}
